60eea69ca49969c3d582a951c318b0b5f80745fb
WalterNights <walternights@gmail.com>
2025-05-22 22:19:56 -0500

    make a function to bulk create books

--- library_app/api_urls.py
+++ library_app/api_urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('books/<int:pk>/', api_views.BookDetailAPIView.as_view(), name='api_book_detail'),
     path('books/<int:pk>/lend/', api_views.lend_book, name='api_lend_book'),
     path('books/<int:pk>/return/', api_views.return_book, name='api_return_book'),
+    path('books/bulk_upload/', api_views.bulk_upload_books, name='api_bulk_upload_books'),
 
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
--- library_app/api_views.py
+++ library_app/api_views.py
@@ -1,8 +1,10 @@
+import pandas as pd
 from .models import Book
+from rest_framework import status
 from .serializers import BookSerializer
 from rest_framework.response import Response
 from rest_framework import generics, permissions
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.decorators import api_view, permission_classes
 
 class BookListCreateAPIView(generics.ListCreateAPIView):
@@ -29,7 +31,7 @@ def lend_book(request, pk):
     user = request.user
     book = Book.objects.get(pk=pk)
     user.borrowed_book.add(Book)
-    return Response({'mensaje': f'{book.title} prestado correctamente.'})
+    return Response({'mensaje': f'{book.title} Prestado correctamente.'})
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
@@ -37,4 +39,26 @@ def return_book(request, pk):
     user = request.user
     book = Book.objects.get(pk=pk)
     user.borrowed_book.remove(Book)
-    return Response({'mensaje': f'{book.title} devuelto correctamente.'})
+    return Response({'mensaje': f'{book.title} Devuelto correctamente.'})
+
+@api_view(['POST'])
+@permission_classes([IsAdminUser])
+def bulk_upload_books(request):
+    excel_file = request.FILES.get('file')
+    if not excel_file:
+        return Response({'error': 'No se ha proporcionado un archivo Excel.'}, status=status.HTTP_400_BAD_REQUEST)
+    try:
+        df = pd.read_excel(excel_file)
+        books = [
+            Book(
+                title=row['titulo'],
+                author=row['autor'],
+                year_publication=row['año de publicación'],
+                stock=row['cantidad disponible']
+            )
+            for _, row in df.iterrows()
+        ]
+        Book.objects.bulk_create(books)
+        return Response({'mensaje': 'Libros subidos correctamente.'}, status=status.HTTP_201_CREATED)
+    except Exception as e:
+        return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
